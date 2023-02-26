4c3828ff60fd6230acf8c6b5ca44c8264464b8c2
Adarsh Hatkar <80952515+AdarshHatkar@users.noreply.github.com>
2023-02-26 10:58:49 +0530

    added gradel also

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ ENV NODE_ENV=production
 WORKDIR /usr/src/app
 COPY ["package.json", "package-lock.json*",  "./"]
 RUN npm install --production --silent 
-RUN apk add openjdk11
+RUN apk add gradle
 COPY . .
 EXPOSE 3000
 RUN chown -R node /usr/src/app
--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+import { exec } from 'child_process';
+
 console.log("hello world");
+
+exec('gradle --version', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+
+  console.log(`stdout: ${stdout}`);
+  console.error(`stderr: ${stderr}`);
+});

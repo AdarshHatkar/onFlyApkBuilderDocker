2d49f88b67c5fa73fc788630e27dbc52f926179e
Adarsh Hatkar <80952515+AdarshHatkar@users.noreply.github.com>
2023-02-26 11:08:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ import { exec } from 'child_process';
 
 console.log("hello world");
 
-exec('gradle --version', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
+// exec('gradle --version', (error, stdout, stderr) => {
+//   if (error) {
+//     console.error(`exec error: ${error}`);
+//     return;
+//   }
 
-  console.log(`stdout: ${stdout}`);
-  console.error(`stderr: ${stderr}`);
-});
+//   console.log(`stdout: ${stdout}`);
+//   console.error(`stderr: ${stderr}`);
+// });
